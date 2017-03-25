1481c0dcac67f45de92f364702d5e069c3f9bf33
coco <cococardry@163.com>
2017-03-26 06:07:05 +0800

    get weather data from juhe api

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,16 @@
+class CitiesController < ApplicationController
+  def index
+    @cities=City.all
+  end
+
+  def update_temp
+    cc=City.find(params[:id])
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                                  :params => { :cityname => cc.juhe_id, :key => JUHE_CONFIG["api_key"] }
+        data = JSON.parse(response.body)
+
+        cc.update( :current_temp => data["result"]["sk"]["temp"] )
+
+        redirect_to cities_path
+  end
+end
